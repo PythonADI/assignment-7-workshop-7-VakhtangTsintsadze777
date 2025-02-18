0d48412efe16bb2cde96204a3b4b2057f8947006
VakhtangTsintsadze777 <vaxo19902010@gmail.com>
2025-02-18 14:49:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def get_word_count():
-     message = "words that are separated by spaces"
-     for "4" in       :
-        print(1)
-get_word_count()
+# 1
+
+message = "words that are separated by spaces"
+
+def get_word_count(words):
+     return words.count(" ") + 1
+
+print(get_word_count(message))
+
+# 2
+MESSAGE_COST_PER_WORD = 0.1
+def get_price(words):
+        return words.count(" ") + 1
+
+print(int(f"price is {MESSAGE_COST_PER_WORD * get_price(message)}$ "))
+
